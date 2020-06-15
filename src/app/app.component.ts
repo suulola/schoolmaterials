587de5a6b50c79f10e5c431fa77c2a6df351ed2b
Oluwaseyi Peter Suulola <oluwaseyisuulola@gmail.com>
2020-06-14 18:02:47 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit  {
+export class AppComponent {
 
   JSSHTMLData: any = {};
   SSHTMLData: any = {};
